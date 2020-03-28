0b1772de4c64a0d1ef1b3f7eabdda8675b4c28a5
Student_M17 <student_m17@ps-dal-m17.home>
2020-03-28 16:20:33 -0500

    more comments

--- src/App.js
+++ src/App.js
@@ -18,27 +18,31 @@ export default class App extends Component {
 
   // creates arrow function called changeColors
   changeColor = () => {
-    
+    // checks if ckickerCount is even
     if (this.state.clickCounter%2===0) {
+      // changing the state by using setState method
       this.setState({
         color: 'orange',
         even: false,
         clickCounter: this.state.clickCounter+1
       })
-    }else {
+    }else { // else if it's not even execute else block
       this.setState({
         color: 'blue',
         even: true,
         clickCounter: this.state.clickCounter+1
       })
     }
+    // logs to console 
     console.log("Click counter===>", this.state.clickCounter)
   }
 
+  // render method and return renders block 
   render() {
     return (
       <div className="App">
         <h1>Box color change</h1>
+        {/* boxcolor sets backgroundcolor and onClick calls the changeColor function */}
         <div className="boxcolor" style={{backgroundColor: this.state.color}} onClick={this.changeColor} >
           <p>CLICK ME!</p>
         </div>
