50ac9524355bff398163807b8d61457689e61f7b
Student_M17 <student_m17@ps-dal-m17.home>
2020-03-28 14:30:44 -0500

    render method

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      color:'blue',
+      clickCounter: 0,
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Box color change</h1>
+        <div className="boxcolor">
+          <p>CLICK ME!</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
